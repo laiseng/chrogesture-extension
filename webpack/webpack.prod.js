a858f40a69073cb0fdf467a040d61555584691d9
Lai Seng <glaiseng@gmail.com>
2021-03-02 15:48:52 +0800

    chore: remove more unused dependencies and upgrade and fix critical dependencies vulnerability

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,8 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const common = require('./webpack.common.js');
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: 'production'
+  mode: "production",
 });
